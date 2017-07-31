68383a3459619d9d9ae93bbebeb71642a73f2f2c
takutotacos <s.takuto0214@gmail.com>
2017-08-01 07:43:28 +0900

    プッシュもプルもできないから、この履歴たちもコミットしてみる。

--- api_test/settings.py
+++ api_test/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*y2n61h02)_m#nlbhjdyan8zl+85jo2mfecv1ks@knl0jc_rxl'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1']
 
 
 # Application definition
